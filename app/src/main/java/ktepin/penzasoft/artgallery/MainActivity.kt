1221d5784fa63374548307ad0bfb3a7c887e1f5c
aiko135 <pon4ik2282211@gmail.com>
2023-08-17 18:13:08 +0300

    Libs updated, target SDK set

--- app/src/main/java/ktepin/penzasoft/artgallery/MainActivity.kt
+++ app/src/main/java/ktepin/penzasoft/artgallery/MainActivity.kt
@@ -3,12 +3,15 @@ package ktepin.penzasoft.artgallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import ktepin.penzasoft.artgallery.ui.theme.ArtgalleryTheme
 
@@ -16,16 +19,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Greeting("Hellow")
-            ArtgalleryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            Column(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Card{
                     Greeting("Android")
+                    Greeting("dev")
                 }
+
             }
+
+
+//            Greeting("Hellow")
+//            ArtgalleryTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Column{
+//                        Greeting("Android")
+//                        Greeting("dev")
+//                    }
+//                }
+//            }
         }
     }
 }
@@ -39,6 +59,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ArtgalleryTheme {
-        Greeting("Android")
+        Greeting("world")
     }
 }
