cfeb42cc971720be3dd7eaadcc69552df08d74bf
aiko135 <pon4ik2282211@gmail.com>
2023-08-18 17:18:59 +0300

    WIP

--- app/src/main/java/ktepin/penzasoft/artgallery/DiModule.kt
+++ app/src/main/java/ktepin/penzasoft/artgallery/DiModule.kt
@@ -1,6 +1,18 @@
 package ktepin.penzasoft.artgallery
+import ktepin.penzasoft.artgallery.data.network.ImageApi
+import ktepin.penzasoft.artgallery.data.network.RetrofitClient
+import ktepin.penzasoft.artgallery.data.repository.ImageRepository
+import ktepin.penzasoft.artgallery.domain.usecase.GetImagePageUseCase
 import org.koin.dsl.module
 
 val diModule = module{
 
+    single<ImageRepository>{
+        val api = RetrofitClient.getTypedRetrofitInstance(ImageApi::class.java)
+        ImageRepository(api)
+    }
+
+    single<GetImagePageUseCase> {
+        GetImagePageUseCase(get<ImageRepository>())
+    }
 }
