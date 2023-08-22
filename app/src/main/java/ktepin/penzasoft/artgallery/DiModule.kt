b392db293f846a494e7826dd2cdfe305f7545052
aiko135 <pon4ik2282211@gmail.com>
2023-08-22 23:27:01 +0300

    Cleanup

--- app/src/main/java/ktepin/penzasoft/artgallery/DiModule.kt
+++ app/src/main/java/ktepin/penzasoft/artgallery/DiModule.kt
@@ -2,7 +2,6 @@ package ktepin.penzasoft.artgallery
 import ktepin.penzasoft.artgallery.data.network.ImageApi
 import ktepin.penzasoft.artgallery.data.network.RetrofitClient
 import ktepin.penzasoft.artgallery.data.repository.ImageRepository
-import ktepin.penzasoft.artgallery.data.repository.LocalImageRepository
 import ktepin.penzasoft.artgallery.domain.usecase.GetImagePageUseCase
 import org.koin.dsl.module
 
